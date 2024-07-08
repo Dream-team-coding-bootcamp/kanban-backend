5ddb75a07aabc69ae7cdee104be1b7467d85faac
Joseph Martinez <insertepala@gmail.com>
2024-07-08 04:37:32 -0500

    Added authorization

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+
 import authRouter from './routes/auth.js'
 import boardRouter from './routes/board.js'
 import projectRouter from './routes/project.js'
 import taskRouter from './routes/task.js'
+
+import auth from './middleware/auth.js'
 
 dotenv.config()
 
@@ -12,6 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/auth', authRouter)
+app.use(auth)
 app.use('/board', boardRouter)
 app.use('/project', projectRouter)
 app.use('/task', taskRouter)
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import { register, login, update } from '../controllers/authController.js'
 import validate from '../middleware/validate.js'
 import { userRegisterSchema, userLoginSchema, userUpdateSchema } from '../schemas/userSchema.js'
+import auth from '../middleware/auth.js'
 
 const router = Router()
 
 router.post('/register', validate(userRegisterSchema), register)
 router.post('/login', validate(userLoginSchema), login)
+router.use(auth)
 router.put('/update/:id', validate(userUpdateSchema), update)
 
 export default router
