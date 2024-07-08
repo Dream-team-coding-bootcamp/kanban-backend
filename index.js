09a83639e570096fa6252b35a2e7dcdea66c2dd4
stivenkno <stivencano931@gmail.com>
2024-07-07 22:06:00 -0500

    change index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
-// import routerBoard from './routes/routerBoard.js'
-import router from './routes/routerBoard.js'
 
 dotenv.config()
 
 const app = express()
-
-app.use(express.json())
-
-app.use('/boards', router)
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
