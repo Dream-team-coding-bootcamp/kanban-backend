0fbb52e5bff833524c9fb93c6f798c097cfbad6f
Jeffer512 <140467995+Jeffer512@users.noreply.github.com>
2024-07-07 22:06:09 -0500

    Changed index.js

--- database/projects.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE projects (
-    project_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255)
-)
--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import router from './routes/projects.js'
 
 dotenv.config()
 
 const app = express()
-
-app.use(express.json())
-
-app.use('/projects', router)
 
 app.listen(3000, () => {
   console.log(`Server is running on port ${process.env.PORT}`)
