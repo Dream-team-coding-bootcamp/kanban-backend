96e2f70e7f6fc35db9c5058d58300d41a40eede6
stivenkno <stivencano931@gmail.com>
2024-07-08 03:26:20 -0500

    preparated for merge

--- pg.js
+++ pg.js
@@ -29,3 +29,4 @@ const checkConnection = async () => {
 checkConnection()
 
 export default pool
+// Se hacen consultas a la base de datos con el objeto pool.query() , y es una promesa por lo tanto se usa await
