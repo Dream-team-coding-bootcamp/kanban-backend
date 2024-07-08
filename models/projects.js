d9264fc66a82e2905338ca909b2b523b5559b193
stivenkno <stivencano931@gmail.com>
2024-07-07 21:43:41 -0500

    refactored jefferson code

--- models/projects.js
+++ models/projects.js
@@ -1,42 +1,42 @@
-import { pool } from "../db.js";
-export class Projects {
+import pool from '../pg.js'
+export default class Projects {
   static async getProjects ({ user_id }) {
     const result = await pool.query(
-      "SELECT * FROM projects WHERE user_id = $1 ORDER BY user_id ASC",
+      'SELECT * FROM projects WHERE user_id = $1 ORDER BY user_id ASC',
       [user_id]
-    );
-    return result.rows;
+    )
+    return result.rows
   };
 
   static async getProjectById ({ user_id, project_id }) {
     const result = await pool.query(
-      "SELECT * FROM projects WHERE user_id = $1 AND project_id = $2", 
+      'SELECT * FROM projects WHERE user_id = $1 AND project_id = $2',
       [user_id, project_id]
-    );
-    return result.rows[0];
+    )
+    return result.rows[0]
   };
 
   static async createProject ({ user_id, name, description }) {
     const result = await pool.query(
-      "INSERT INTO projects (user_id, name, description) VALUES ($1, $2, $3)",
+      'INSERT INTO projects (user_id, name, description) VALUES ($1, $2, $3)',
       [user_id, name, description]
-    );
+    )
     return result.rows[0]
   };
 
   static async updateProject ({ name, description, user_id, project_id }) {
     const result = await pool.query(
-      "UPDATE projects SET name = $1, description = $2 WHERE user_id = $3 AND project_id = $4",
+      'UPDATE projects SET name = $1, description = $2 WHERE user_id = $3 AND project_id = $4',
       [name, description, user_id, project_id]
-    );
-    return result.rows[0];
+    )
+    return result.rows[0]
   };
 
   static async deleteProject ({ user_id, project_id }) {
     const { rowCount } = await pool.query(
-      "DELETE FROM projects WHERE user_id = $1 AND project_id = $2", 
+      'DELETE FROM projects WHERE user_id = $1 AND project_id = $2',
       [user_id, project_id]
-    );
-    return rowCount;
+    )
+    return rowCount
   };
 }
--- routes/projects.js
+++ routes/projects.js
@@ -1,12 +1,12 @@
-import { Router } from "express";
-import { ProjectsController } from "../controllers/projects";
+import { Router } from 'express'
+import { ProjectsController } from '../controllers/projects.js'
 
-const router = Router();
+const router = Router()
 
-router.get("/:user_id/projects", ProjectsController.getProjects);
-router.get("/:user_id/projects/:project_id", ProjectsController.getProjectById);
-router.post("/:user_id/projects", ProjectsController.createProject);
-router.put("/:user_id/projects/:project_id", ProjectsController.updateProject);
-router.delete("/:user_id/projects/:project_id", ProjectsController.deleteProject);
+router.get('/:user_id', ProjectsController.getProjects)
+router.get('/:user_id/:project_id', ProjectsController.getProjectById)
+router.post('/:user_id', ProjectsController.createProject)
+router.put('/:user_id/:project_id', ProjectsController.updateProject)
+router.delete('/:user_id/:project_id', ProjectsController.deleteProject)
 
-export default router;
+export default router
