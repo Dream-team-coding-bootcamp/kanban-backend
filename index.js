820b04c51eaf18ce97380896c103bcda38626e02
sebas1705sanchez <151795806+sebas1705sanchez@users.noreply.github.com>
2024-07-07 22:05:51 -0500

    changed index.js

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import userRouter from './routes/authRoutes.js' // Asegúrate de que la ruta al archivo user.js sea correcta
 
 dotenv.config()
 
 const app = express()
-app.use(bodyParser.json()); // procesamiento de datos en formato JSON del body
-//app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
-app.use('/user', userRouter)
-
-app.listen(5000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 5000}`)
+app.listen(3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`)
 })
